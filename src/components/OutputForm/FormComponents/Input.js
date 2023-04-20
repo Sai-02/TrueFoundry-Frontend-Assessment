0249584671c064638f821b8f9899d34a28bfa571
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2023-04-21 01:07:16 +0530

    [add] : basic Input tag added

--- src/components/OutputForm/FormComponents/Input.js
+++ src/components/OutputForm/FormComponents/Input.js
@@ -1,5 +1,6 @@
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Tooltip from "@mui/material/Tooltip";
 import React from "react";
 
 const Input = ({ val }) => {
@@ -9,11 +10,22 @@ const Input = ({ val }) => {
   return (
     <>
       <div className="flex gap-4 space-between justify-center items-center px-4">
-        <label htmlFor="" className="">
+        <label htmlFor="" className="flex gap-2 items-center">
           {val?.label}
-          {isDescriptionValid() ? <FontAwesomeIcon icon={faInfoCircle} /> : ""}
+          {isDescriptionValid() ? (
+            <Tooltip title={val.description}>
+              <FontAwesomeIcon icon={faInfoCircle} className="cursor-pointer" />
+            </Tooltip>
+          ) : (
+            ""
+          )}
         </label>
-        <input type="text" className="border-2 outline-none p-2 rounded" />
+        <input
+          type="text"
+          className="border-2 outline-none p-2 rounded"
+          placeholder={val?.placeholder}
+          name={val?.jsonKey}
+        />
       </div>
     </>
   );
